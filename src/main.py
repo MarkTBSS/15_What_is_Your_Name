a3472460a9e683a0b1b6082de98a58135cf73fa4
MarkTBSS <marktbss@gmail.com>
2023-10-19 07:07:55 +0700

    [ADD] Python File

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def print_full_name(first, last):
+    #print(first)
+    #print(type(first))
+    #print(last)
+    #print(type(last))
+    #print(f"Hello {first} {last}! You just delved into python.")
+    print("Hello " + first + " " + last + "! You just delved into python.")
+
+first = "Ross"
+last = "Taylor"
+print_full_name(first, last)
